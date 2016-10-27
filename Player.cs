6bbfd6a0f84fdce8700cd3bb221e5c808dc81cd8
Hede93 <hede93@live.dk>
2016-10-27 14:33:39 +0200

    Player rank change

--- Player.cs
+++ Player.cs
@@ -11,33 +11,15 @@ namespace database
 {
 	public class Player
 	{
-		private int _id;
-		private string _username;
-		private int _points;
 
-		public int Id
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public int Rank { get; set; }
+
+
+		public Player (int id, string username, int rank)
 		{
-			get { return _id; }
-			set { _id = value; }
-		}
 			
-		public string Username
-		{
-			get { return _username; }
-			set { _username = value; }
-		}	
-
-		public int Points
-		{
-			get { return _points; }
-			set { _points = value; }
-		}
-
-		public Player (int id, string username, int points)
-		{
-			_id = id;
-			_username = username;
-			_points = points;
 		}
 	}
 }
